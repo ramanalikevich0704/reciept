7c521679309b8994abb733f9b3ddc99651779905
user <user@users-MacBook-Pro.local>
2026-02-17 08:50:53 +0100

    Login-1 developer login view with validation logic

--- reciept/app/(tabs)/index.tsx
+++ reciept/app/(tabs)/index.tsx
@@ -1,6 +1,7 @@
-// import { Button } from '@react-navigation/elements';
-import { Stack } from "expo-router";
-import { useState } from "react";
+import { Styles } from "@/components/login/LoginStyles";
+import { yupResolver } from '@hookform/resolvers/yup';  
+import * as yup from 'yup';
+
 import {
   TouchableOpacity,
   Image,
@@ -11,58 +12,72 @@ import {
   View,
 } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
-import { useForm, Controller } from "react-hook-form";
+import {
+  useForm,
+  Controller,
+} from "react-hook-form";
+
+interface LoginForm { email: string; password: string; } 
 
 export default function LoginView() {
   const bgImage = require("@/assets/images/login-background.jpg");
   const emailRegex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
-  
-  const {
-    control,
-    handleSubmit,
-    formState: { isValid, errors },
-  } = useForm({
-    mode:"onChange",
-    defaultValues: {
-      email: "",
-      password: "",
-    },
+
+  const loginSchema = yup.object({  
+    email: yup.string()
+           .min(2, 'Введите более двух символов')
+           .matches(/[@]/, "Необходимо написать @")  
+           .matches(/[.]/, "Допишите почтовый домен")
+           .email('Поле email не соответствует формату'), 
+    password: yup.string()
+              .required()
+              .min(8, 'Введите 8 и более символов в пароле')
+              .matches(/(?=.*\d)/, "Введите хотя бы одну цифру")  
+              .matches(/(?=.*[A-Za-z])/, "Введите хотя бы одну букву")  
+              .matches(/[@$!%*?&)()]/, "Введите хотя бы один символ") 
+              .max(256, 'Максимальное количество символов в пароле 256, уберите лишние символы') 
+  }).required();  
+
+  // const {
+  //   control,
+  //   handleSubmit,
+  //   formState: { isValid, errors },
+  // } = useForm({
+  //   mode: "onChange",
+  //   defaultValues: {
+  //     email: "",
+  //     password: "",
+  //   },
+  // });
+  const { control, handleSubmit, formState: { isValid, errors } } = useForm<LoginForm>({ 
+    mode: "onChange", 
+    resolver: yupResolver(loginSchema)
   });
-  const onSubmit = () => console.log("dsd");//вызов запроса в сеть
+
+  const onSubmit = () => console.log("dsd");
 
   return (
-    <View style={{ height: "100%", width: "100%", backgroundColor: "black" }}>
+    <View style={Styles.mainBackground}>
       <ImageBackground
         source={bgImage}
         resizeMode="cover"
-        style={styles.background}
-        imageStyle={{ flex: 1, backgroundColor: "rgba(156, 148, 148, 0.5)" }}
+        style={Styles.imageBackgroundStyle}
+        imageStyle={Styles.imageBackgroundImageStyle}
       >
-        <View
-          style={[
-            StyleSheet.absoluteFillObject,
-            { backgroundColor: "rgba(25,25,25,0.4)" },
-          ]}
-        />
-        <SafeAreaView style={{ flex: 1, justifyContent: "space-around" }}>
+        <View style={[
+          StyleSheet.absoluteFillObject,
+          Styles.blurColor
+        ]} />
+        <SafeAreaView style={Styles.safeArea}>
           <View style={{ marginTop: 44 }}>
             <View>
-              <Text
-                style={{
-                  fontSize: 18,
-                  fontFamily: "SF Pro Display",
-                  color: "white",
-                }}
-              >
+              <Text style={[
+                Styles.whiteText,
+                Styles.mediumStandardText
+                ]}>
                 Welcome to
               </Text>
-              <Text
-                style={{
-                  fontSize: 36,
-                  fontFamily: "Apercu Pro",
-                  color: "white",
-                }}
-              >
+              <Text style={[Styles.whiteText, Styles.largeCustomText]}>
                 Recipe Book
               </Text>
             </View>
@@ -72,23 +87,19 @@ export default function LoginView() {
               <Controller
                 control={control}
                 rules={{
-                  required: true,
-                  validate: (value) => {
-
-                    const isValid = emailRegex.test(value)
-                    if (value.length <= 2) return "Введите более двух символов"
-                    if (!value.includes('@')) return "Необходимо написать @"
-                     if (!value.includes('.')) return "Допишите почтовый домен"
-                    if (!isValid) return "Поле email не соответствует формату";
-                    console.log("все ок")
-                    return true
-                  }
+                  required: true
                 }}
                 render={({ field: { onChange, onBlur, value } }) => (
                   <TextInput
-                    style={styles.input}
-                    placeholder="Enter email"
-                    placeholderTextColor="rgba(255, 255, 255, 1)"
+                    style={[
+                            Styles.field,
+                            Styles.element,
+                            Styles.ordinaryCustomText,
+                            Styles.fieldForm,
+                            {marginBottom: 12}
+                          ]}
+                    placeholder={"Enter email"}
+                    placeholderTextColor={Styles.whiteText.color}
                     onBlur={onBlur}
                     onChangeText={onChange}
                     value={value}
@@ -96,100 +107,103 @@ export default function LoginView() {
                 )}
                 name="email"
               />
-              {errors.email && errors.email.message && (
-                <Text style={[styles.error]}>
+              {errors['email'] && errors['email'].message && (
+                <Text style={[Styles.errorText]}>
                   {errors.email.message}
                 </Text>
               )}
               <Controller
                 control={control}
                 rules={{
-                  required: true,
-                  validate: (value) => {
-                    console.log(value)
-                    if (value.length < 8) return "Введите 8 и более символов в пароле"
-                    if (!/(?=.*\d)/.test(value)) return "Введите хотя бы одну цифру"
-                    if (!/(?=.*[A-Za-z])/.test(value)) return "Введите хотя бы одну букву"
-                    if (!/[@$!%*?&)()]/.test(value)) return "Введите хотя бы один символ"
-                    if (value.length >= 256) return "Максимальное количество символов в пароле 256, уберите лишние символы"
-                    return true
-                  }
+                  required: true
                 }}
                 render={({ field: { onChange, onBlur, value } }) => (
                   <TextInput
-                    style={styles.input}
-                    placeholder="Enter password"
-                    placeholderTextColor="rgba(255, 255, 255, 1)"
+                    style={[
+                            Styles.field,
+                            Styles.element,
+                            Styles.ordinaryCustomText,
+                            Styles.fieldForm
+                          ]}
+                    placeholder={"Enter password"}
+                    placeholderTextColor={Styles.whiteText.color}
                     onBlur={onBlur}
                     onChangeText={onChange}
                     value={value}
-                    secureTextEntry={true}
+                    secureTextEntry
                   />
                 )}
                 name="password"
               />
-              {errors.password && errors.password.message && (
-                <Text style={[styles.error]}>
+              {errors['password'] && errors['password'].message && (
+                <Text style={[Styles.errorText]}>
                   {errors.password.message}
                 </Text>
               )}
             </View>
             <Text
-              style={styles.forgetPassword}
+              style={Styles.forgetPassword}
               onPress={() => alert("Текст нажат!")}
             >
               Forget password?
             </Text>
-            {console.log(isValid)}
-            {console.log(errors.email)}
-            {console.log(errors.password)}
             <TouchableOpacity
-              style={[styles.button, !isValid && styles.buttonDisabled]}
+              style={[
+                Styles.button,
+                Styles.centerPosition,
+                Styles.element,
+                !isValid && Styles.buttonDisabled,
+                Styles.centerPosition,
+                Styles.element
+              ]}
               disabled={!isValid}
               onPress={handleSubmit(onSubmit)}
               // onPress={() => alert("Текст нажат!")}
             >
               <Text
                 style={[
-                  styles.greenText,
-                  isValid && styles.disableButtonText,
+                  Styles.greenText,
+                  isValid && Styles.disableButtonText
                 ]}
               >
                 Login
               </Text>
             </TouchableOpacity>
-            <Text style={styles.loginwith}>Login with</Text>
-            <View
-              style={{
-                flexDirection: "row",
-                justifyContent: "center",
-                gap: 15,
-              }}
-            >
+            <Text style={Styles.loginwith}>Login with</Text>
+            <View style={Styles.socialNetworkContainer}>
               <TouchableOpacity
-                style={[styles.socialNetworkButton, styles.whitebutton]}
+                style={[
+                  Styles.socialNetworkButton, 
+                  Styles.whitebutton
+                ]}
                 onPress={() => console.log("Нажато!")}
               >
                 <Image
                   source={require("@/assets/images/google.png")}
-                  style={styles.icon}
+                  style={Styles.icon}
                 />
               </TouchableOpacity>
               <TouchableOpacity
-                style={[styles.socialNetworkButton, styles.bluebutton]}
+                style={[
+                  Styles.socialNetworkButton,
+                  Styles.bluebutton
+                ]}
                 onPress={() => console.log("Нажато!")}
               >
                 <Image
                   source={require("@/assets/images/facebook.png")}
-                  style={styles.icon}
+                  style={Styles.icon}
                 />
               </TouchableOpacity>
             </View>
-            <Text style={styles.loginwith}>or</Text>
-            <TouchableOpacity
-              style={[styles.greenButton]}
-            >
-              <Text style={styles.whiteText}>Create an account</Text>
+            <Text style={Styles.loginwith}>or</Text>
+            <TouchableOpacity 
+            style={[
+              Styles.registerButton, 
+              Styles.centerPosition,
+              Styles.element
+            ]}>
+              <Text style={[Styles.mediumCustomText, Styles.boldCustomText, Styles.whiteText]}>Create an account</Text>
             </TouchableOpacity>
           </View>
         </SafeAreaView>
@@ -197,108 +211,6 @@ export default function LoginView() {
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  input: {
-    height: 45,
-    // margin: 12,
-    // marginVertical: 12,
-    borderWidth: 1,
-    padding: 18,
-    borderColor: "white",
-    color: "white",
-    borderRadius: 10,
-    backgroundColor: "rgba(244, 235, 235, 0.45)",
-    marginTop: 12,
-    // display: showText ? 'flex' : 'none'
-  },
-  background: {
-    flex: 1, // Растягивает на весь экран
-    paddingHorizontal: 20,
-  },
-  error: {
-    color: "white",
-    fontSize: 12,
-    fontWeight: "bold",
-    backgroundColor: "rgba(237, 7, 7, 0.5)",
-    borderRadius: 8,
-    paddingHorizontal: 18,
-  },
-  button: {
-    height: 45,
-    color: "rgba(52, 168, 83, 1)",
-    backgroundColor: "white",
-    borderRadius: 10,
-    justifyContent: "center",
-    alignItems: "center",
-  },
-  greenText: {
-    color: "rgba(52, 168, 83, 1)",
-    fontSize: 16,
-    fontWeight: 700,
-    fontFamily: "Apercu Pro",
-  },
-  whiteText: {
-    color: "white",
-    fontSize: 16,
-    fontWeight: 700,
-    fontFamily: "Apercu Pro",
-  },
-  disableButtonText: {
-    color: "rgba(74, 5, 5, 0.45)",
-    fontSize: 18,
-    fontFamily: "Apercu Pro",
-  },
-  buttonDisabled: {
-    backgroundColor: "#A9A9A9", // Серый для неактивной
-    opacity: 0.7,
-    color: "white",
-  },
-  forgetPassword: {
-    fontSize: 14,
-    fontFamily: "Apercu Pro",
-    fontWeight: 700,
-    textDecorationLine: "underline",
-    color: "white",
-    textAlign: "right",
-    paddingBottom: 18,
-  },
-  loginwith: {
-    fontSize: 14,
-    fontFamily: "Apercu Pro",
-    fontWeight: 700,
-    color: "white",
-    textAlign: "center",
-    paddingBottom: 18,
-    paddingVertical: 12,
-  },
-  icon: {
-    width: 20,
-    height: 20,
-    marginRight: 8,
-  },
-  bluebutton: {
-    backgroundColor: "rgba(59, 89, 153, 1)",
-    borderRadius: 10,
-  },
-  whitebutton: {
-    backgroundColor: "white",
-    borderRadius: 10,
-  },
-  socialNetworkButton: {
-    height: 45,
-    width: 145,
-    justifyContent: "center",
-    alignItems: "center",
-  },
-  greenButton: {
-    height: 45,
-    justifyContent: "center",
-    alignItems: "center",
-    borderRadius: 10,
-    backgroundColor: "rgba(52, 168, 83, 1)",
-  },
-});
 
 //react hook form почитать
 // react/react-native под капотом
